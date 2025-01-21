5cd9a430057cf5b3b2749ce5a763814950f24b93
hyejj19@naver.com <hyejj19@naver.com>
2025-01-21 23:20:35 +0900

    add & refactor: navigation logic, refactor folder structure
    - screens -> features

--- lib/features/auth/sign_up_screen.dart
+++ lib/features/auth/sign_up_screen.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/auth/login_screen.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
+
+  void onLoginTap(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => LoginScreen(),
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,11 +51,14 @@ class SignUpScreen extends StatelessWidget {
           children: [
             Text('Already have an account?'),
             Gaps.h5,
-            Text(
-              'Login',
-              style: TextStyle(
-                color: Theme.of(context).primaryColor,
-                fontWeight: FontWeight.bold,
+            GestureDetector(
+              onTap: () => onLoginTap(context),
+              child: Text(
+                'Login',
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
           ],
