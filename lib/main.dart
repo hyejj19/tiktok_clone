0e8d4531873a3779ae4f64966266edfb588e290f
hyejj19@naver.com <hyejj19@naver.com>
2025-01-22 22:00:35 +0900

    add: theme for scaffold & app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/auth/sign_up_screen.dart';
 
 void main() {
@@ -13,9 +14,17 @@ class TiktokApp extends StatelessWidget {
     return MaterialApp(
         title: 'Tiktok clone',
         theme: ThemeData(
-          primaryColor: const Color(0xFF5D8736),
-          scaffoldBackgroundColor: Colors.white,
-        ),
+            primaryColor: const Color(0xFF5D8736),
+            scaffoldBackgroundColor: Colors.white,
+            appBarTheme: AppBarTheme(
+                foregroundColor: Colors.black,
+                backgroundColor: Colors.white,
+                elevation: 0,
+                titleTextStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: Sizes.size16 + Sizes.size2,
+                  fontWeight: FontWeight.w500,
+                ))),
         home: SignUpScreen());
   }
 }
